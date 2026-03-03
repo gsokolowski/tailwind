e84d23625d057dd76fe2a454de3a51bc69594a4d
gsokolowski <gregor.sokolowski@gmail.com>
2026-03-03 18:57:51 +0100

    using grid

--- tailwind-react/src/App.jsx
+++ tailwind-react/src/App.jsx
@@ -1,11 +1,26 @@
+import land from './assets/land-1.png'
+
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
-    </div>
+    <section className='grid gap-8 justify-items-center'>
+      <div className='px-5'>
+        <img 
+          src={land} 
+          alt="Land" 
+          className="w-full max-w-[400] h-auto rounded-lg shadow-lg" 
+        />
+      </div>
+      <div className='px-5'>
+        <h1 className='text-4xl font-medium'>Headline</h1>
+        <p className="my-5">
+          Morbi non suscipit felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sed dui blandit, faucibus mauris id, laoreet elit. Cras dictum ultrices iaculis. In imperdiet euismod egestas. Phasellus placerat eleifend lacus, vel porta enim volutpat non. Quisque suscipit sit amet nisi id maximus. Curabitur id venenatis velit, et congue leo. Nulla scelerisque feugiat enim non suscipit.
+        </p>
+        <p className="">
+          Nam purus sapien, tincidunt ac nisi vitae, fringilla condimentum tortor. Morbi elementum venenatis turpis, eu pharetra odio porttitor sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sem leo, rutrum non turpis quis, faucibus ullamcorper quam. Integer sit amet auctor nulla. Maecenas convallis condimentum ipsum, quis pharetra ex. Sed pulvinar eget augue et porttitor. Integer fermentum diam in magna dapibus bibendum. Quisque quis ipsum lectus. 
+        </p>
+      </div>
+    </section>
   )
 }
 
